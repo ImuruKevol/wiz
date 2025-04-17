070b30830af635de59c2ed5b1d442d9611a0ed1f
KwonTaewook <nanoha@kakao.com>
2025-04-17 15:08:16 +0900

    Update compiler.py

--- src/season/util/compiler.py
+++ src/season/util/compiler.py
@@ -1,6 +1,11 @@
 import inspect
 import season
 
+from flask import request, abort
+import datetime
+import traceback
+import sys
+
 class compiler:
     def __init__(self, fn=None):
         self.fn = fn
@@ -8,10 +13,32 @@ class compiler:
     def build(self, code, name=None, logger=print, **kwargs):
         fn = {'__file__': name, '__name__': name, 'print': logger, 'season': season}
         for key in kwargs: fn[key] = kwargs[key]
-        if type(code) == str:
-            exec(compile(code, name, 'exec'), fn)
-        else:
-            exec(code, fn)
+        try:
+            if type(code) == str:
+                exec(compile(code, name, 'exec'), fn)
+            else:
+                exec(code, fn)
+        except season.lib.exception.ResponseException as e:
+            raise season.lib.exception.ResponseException(e.code, e.response)
+        except Exception as e:
+            error_type = type(e).__name__
+            if error_type != "InternalServerError":
+                tb = traceback.TracebackException(*sys.exc_info())
+                filtered = []
+                for entry in tb.stack:
+                    if entry.filename.endswith("/site-packages/season/util/compiler.py"):
+                        continue
+                    filtered.append(f'  File "\033[1m{entry.filename}\033[0m", line \033[1m{entry.lineno}\033[0m, in {entry.name}\n    {entry.line}')
+                if filtered:
+                    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    print("\033[91m[{now}][ERROR][http][error]\033[0m {uri}".format(now=now, uri=request.path))
+                    print("Traceback (most recent call last):")
+                    print("\n".join(filtered))
+                    print(f"{error_type}: {str(e)}")
+                else:
+                    # fallback: show last frame of traceback
+                    traceback.print_exc()
+            abort(500, description="")
         self.fn = fn
         return self
     
